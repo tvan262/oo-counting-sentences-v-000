69cd32c2d647e02047b57017c5953619eb910abf
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-18 17:59:47 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?.
-    
+    if self.end_with? .
+      true
+    else
+      false
   end
 
   def question?
-
+    self.end_with? ?
   end
 
   def exclamation?
+    self.end_with? !
 
   end
 
