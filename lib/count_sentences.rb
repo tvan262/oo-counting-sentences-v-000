a1445fe074c53855acf6a68d487bc271164a0c0f
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-18 18:03:27 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,20 +7,23 @@ class String
       true
     else
       false
+    end
   end
 
   def question?
-    self.end_with?("?")
+    if self.end_with?("?")
      true
     else
       false
+    end  
   end
 
   def exclamation?
-    self.end_with?("!")
+    if self.end_with?("!")
       true
     else
       false
+    end
   end
 
   def count_sentences
